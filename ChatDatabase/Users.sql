f943f41dad88633556cf2e984d8e05134df0806c
AJ van Deventer <andries@firsttech.digital>
2020-10-10 13:08:47 +0200

    Add project files.

---
+++ ChatDatabase/Users.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] VARCHAR(50) NOT NULL, 
+    [Surname] VARCHAR(50) NULL, 
+    [Username] VARCHAR(50) NOT NULL, 
+    [Password] VARCHAR(500) NOT NULL
+)

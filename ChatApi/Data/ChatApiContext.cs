3622acae1c2cae14f7e1f8e9674d08f439d49cf0
AJ van Deventer <andries@firsttech.digital>
2020-10-11 19:32:45 +0200

    Added controller and entity context to API

---
+++ ChatApi/Data/ChatApiContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ChatApi.Models;
+
+namespace ChatApi.Data
+{
+    public class ChatApiContext : DbContext
+    {
+        public ChatApiContext (DbContextOptions<ChatApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ChatApi.Models.Messages> Messages { get; set; }
+    }
+}
---
+++ ChatApi/Models/Messages.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatApi.Models
+{
+    public class Messages
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public string Message { get; set; }
+        public string TimeStamp { get; set; }
+    }
+}

fa271c269d06d1ed5080681810b770a1ec3b49c2
AJ van Deventer <andries@firsttech.digital>
2020-10-11 16:06:02 +0200

    Create UI basic components with models

---
+++ ChatHub/Models/LoginModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatHub.Models
+{
+    public class LoginModel
+    {
+        [StringLength(50, ErrorMessage = "Please fill in your Username")]
+        public string UserName { get; set; }
+
+        [StringLength(500, ErrorMessage = "Please fill in your Password")]
+        public string Password { get; set; }
+
+        [StringLength(50, ErrorMessage = "Please fill in your Name")]
+        public string Name { get; set; }
+
+        [StringLength(50)]
+        public string Surname { get; set; }
+    }
+}
---
+++ ChatHub/Models/UserMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatHub.Models
+{
+    public class UserMessage
+    {
+        public enum MessgageType
+        {
+            Server, User, Othere
+        }
+
+        public DateTime TimeStamp { get; set; }
+        public string Message { get; set; }
+        public string UserName { get; set; }
+        public MessgageType Type { get; set; }
+    }
+}
